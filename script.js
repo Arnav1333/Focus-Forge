d3f1e65dbb4e8b5af6c042cc4569dc908795a01a
Arnav Pawar <109753432+Arnav1333@users.noreply.github.com>
2023-12-18 23:15:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    let nav = document.querySelector('.nav-list');
+    nav.classList.toggle('active');
+}
+function myFunction() {
+    let element = document.body;
+    element.classList.toggle("dark");
+}
